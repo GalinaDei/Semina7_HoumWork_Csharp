764c0d0915f5e681e23c79b85e98f5a0836a46f1
Galina <gal-dej@yandex.ru>
2022-10-21 17:02:23 +0300

    Task has done

--- Task50/Program.cs
+++ Task50/Program.cs
@@ -7,3 +7,37 @@
 8 4 2 4
 17 -> такого числа в массиве нет
 */
+int [,] CreateMatrix (int m, int n)
+{
+  int [,] matrix = new int [m,n];
+  Random rnd = new Random();
+  for (int i = 0; i < m; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i,j]=rnd.Next(1,9);
+    }
+  }
+  return matrix;
+}
+void PrintMatrix (int[,]matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+       for (int j = 0; j < matrix.GetLength(1); j++)
+       {
+        Console.Write($"{matrix[i,j]} ");        
+       }
+       Console.WriteLine(); 
+    }
+}
+
+void ElementSearch (int[,]matrix, int m, int n)
+{
+    if(m>=matrix.GetLength(0) | n>=matrix.GetLength(1))
+    {Console.WriteLine("Такого числа нет");}
+    else {Console.WriteLine($"{matrix[m,n]}");}
+}
+int[,]resultMatrix = CreateMatrix(7,7);
+PrintMatrix(resultMatrix);
+ElementSearch(resultMatrix, 7, 7);

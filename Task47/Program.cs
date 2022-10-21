cc256277a6674dbfb0c309d7a31423424a3fe675
Galina <gal-dej@yandex.ru>
2022-10-21 16:15:48 +0300

    Task has done

--- Task47/Program.cs
+++ Task47/Program.cs
@@ -5,3 +5,30 @@ m = 3, n = 4.
 1 -3,3 8 -9,9
 8 7,8 -7,1 9
 */
+double [,] CreateMatrix (int m, int n)
+{
+  double [,] matrix = new double [m,n];
+  Random rnd = new Random();
+  for (int i = 0; i < m; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i,j]=rnd.Next(1,99)+Math.Round(rnd.NextDouble(),2);
+    }
+  }
+  Console.WriteLine($"m = {m}, n = {n}.");
+  return matrix;
+}
+void PrintMatrix (double[,]matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+       for (int j = 0; j < matrix.GetLength(1); j++)
+       {
+        Console.Write($"{matrix[i,j]} ");        
+       }
+       Console.WriteLine(); 
+    }
+}
+
+PrintMatrix(CreateMatrix(8,6));
